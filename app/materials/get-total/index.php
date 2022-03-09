629bc1dbc5ddba6a904f746eca40d4b3c31d8441
re-knownout <knownout@hotmail.com>
2022-03-09 15:11:37 +0200

    Tag-specific materials total count request

--- app/materials/get-total/index.php
+++ app/materials/get-total/index.php
@@ -3,7 +3,9 @@
 require_once $_SERVER["DOCUMENT_ROOT"] . "/lib/make-output.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/app/make-connection.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/lib/use-cors.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/types/requests.php";
 
+use Types\MaterialSearchRequests;
 use function Lib\makeOutput;
 use function Lib\useCorsHeaders;
 use function Lib\useOutputHeader;
@@ -12,10 +14,15 @@ use function Lib\useOutputHeader;
 useCorsHeaders();
 useOutputHeader();
 
+$tag = $_POST[MaterialSearchRequests::tags];
+
 $database = makeDatabaseConnection();
 if (!$database) exit(makeOutput(false, [ "no-database-connection" ]));
 
-$query = $database->query("select count(*) from materials where datetime < " . time());
+$stringQuery = "select count(*) from materials where datetime < " . time();
+if (isset($tag)) $stringQuery .= " and tags like '%$tag%'";
+
+$query = $database->query($stringQuery);
 if (!$query) exit(makeOutput(false, [ "query-fail" ]));
 
 $materialsCount = $query->fetch_row();

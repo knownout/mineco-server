faaaeaf17ae028e498a9c2b88b96ad46b1453a4a
re-knownout <knownout@hotmail.com>
2022-03-13 00:04:20 +0200

    Minor tweaks

--- app/materials/update/index.php
+++ app/materials/update/index.php
@@ -21,9 +21,9 @@ useCorsHeaders();
 $accountData = verifyAccountData();
 $pathBuilder = new PathBuilder();
 
-$identifier = $_POST[Requests::updateMaterial];
-$content = $_POST[Requests::updateMaterialContent];
-$text = $_POST[Requests::updateMaterialText];
+$identifier = $_POST[ Requests::updateMaterial ];
+$content = $_POST[ Requests::updateMaterialContent ];
+$text = $_POST[ Requests::updateMaterialText ];
 
 if (!$accountData) exit(makeOutput(false, [ "auth-error" ]));
 if (!isset($identifier) or !isset($content)) exit(makeOutput(false, [ "no-identifier" ]));
@@ -52,18 +52,25 @@ $queryString = "insert into materials (" . join(", ", $keysList) . ") values ("
     . " " . join(", ", $queryList);
 
 $query = $database->query($queryString);
-$database->mysqli->close();
 
-if (!$query) exit(makeOutput(false, [ "query-error", $queryString ]));
+if (!$query) {
+    $database->mysqli->close();
+    exit(makeOutput(false, [ "query-error", $queryString ]));
+}
 
 $textFilePath = $pathBuilder->makePath($pathBuilder->materialsStorage, $identifier . ".json");
 file_put_contents($textFilePath, json_encode($text,
     JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT));
 
 
-if ($identifier !== $content["identifier"]) rename(
-    $textFilePath,
-    $pathBuilder->makePath($pathBuilder->materialsStorage, $content["identifier"] . ".json")
-);
+if ($identifier !== $content["identifier"]) {
+    rename(
+        $textFilePath,
+        $pathBuilder->makePath($pathBuilder->materialsStorage, $content["identifier"] . ".json")
+    );
 
-exit(makeOutput(true, [ $content["identifier"] ]));
+    $database->query("delete from materials where identifier=" . $identifier);
+}
+
+$database->mysqli->close();
+exit(makeOutput(true, [ $content["identifier"], "delete from materials where identifier=" . $identifier ]));
